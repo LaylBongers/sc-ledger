fb1675a5f24a6bfac15efd16b80d68cba003c3c8
Layl <LaylConway@users.noreply.github.com>
2018-01-01 03:01:23 +0100

    Add basic price form and log list

---
+++ src/renderer/AddPriceLog.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+    <div class="card bg-dark mt-3">
+        <h3 class="card-header">New Price Log</h3>
+        <div class="card-body">
+            <div class="form-group">
+                <label>Location</label>
+                <input type="text" class="form-control"></input>
+            </div>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Resource</th>
+                        <th scope="col">Buy</th>
+                        <th scope="col">Sell</th>
+                            <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="change in priceLog.changes">
+                        <td>{{ change.resource }}</td>
+                        <td>{{ change.buy }}</td>
+                        <td>{{ change.sell }}</td>
+                            <td><button class="btn btn-danger" style="width: 2.4em;">-</button></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" class="form-control"></input></td>
+                        <td><input type="text" class="form-control"></input></td>
+                        <td><input type="text" class="form-control"></input></td>
+                            <td><button class="btn btn-success" style="width: 2.4em;">+</button></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button type="submit" class="btn btn-primary" v-on:click="submitPricelog">Submit Price Log</button>
+            <p>{{ submittedText }}</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'add-price-log',
+    data () {
+        return {
+            priceLog: {
+                timestamp: new Date(),
+                location: 'Port Olisar',
+                changes: [
+                    {
+                        resource: 'Hydrogen',
+                        buy: 0.8,
+                        sell: null,
+                    },
+                ],
+            },
+            submittedText: 'Not Submitted',
+        }
+    },
+    methods: {
+        submitPricelog () {
+            this.submittedText = 'Submitted'
+        }
+    },
+}
+</script>
+
+<style>
+</style>
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <div id="app">
-      <h1>Ledger</h1>
-  </div>
+<div id="app">
+    <div class="container-fluid text-light">
+        <AddPriceLog />
+
+        <h2 class="mt-3">Price Logs</h2>
+        <div v-for="log in priceLogs">
+            <h3>{{ log.location }} at {{ log.timestamp.toUTCString() }}</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Resource</th>
+                        <th scope="col">Buy</th>
+                        <th scope="col">Sell</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="change in log.changes">
+                        <td>{{ change.resource }}</td>
+                        <td>{{ change.buy }}</td>
+                        <td>{{ change.sell }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import AddPriceLog from './AddPriceLog'
+
 export default {
-    name: 'sc-ledger'
+    name: 'sc-ledger',
+    components: {
+        AddPriceLog
+    },
+    data () {
+        return {
+            priceLogs: [
+                {
+                    timestamp: new Date(),
+                    location: 'Port Olisar',
+                    changes: [
+                        {
+                            resource: 'Hydrogen',
+                            buy: 0.8,
+                            sell: null,
+                        },
+                    ],
+                }
+            ],
+        }
+    },
 }
 </script>
 
 <style>
 @import '~bootstrap/dist/css/bootstrap.css';
+
+body {
+    background-color: #202024;
+}
+
 </style>
